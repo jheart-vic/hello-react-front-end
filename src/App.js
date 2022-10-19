7f50c39193b7063e42402c1c2a86717ead4c1776
jheart-vic <adebowalevictorjheart@gmail.com>
2022-10-19 09:59:03 +0100

    Finish front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
+import store from './store/store';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Greeting />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </div>
+);
 
 export default App;
